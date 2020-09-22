ae8fbbaa228b84faa62f7b4795e15b49a23e23ec
Evgeniy Kozhevnikov <evgeniy.kozhevnikov.place@gmail.com>
2020-09-22 10:22:09 +0300

    добавил пару компонент

---
+++ Assets/_MyScripts/Events/MouseClickEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MyEngine
+{
+    // создаёт событие по клику на объекте
+    [RequireComponent(typeof(Collider))]
+    public class MouseClickEvent : MonoBehaviour
+    {
+        public float DelayClick = 1f;
+        public UnityEvent OnMouseClick;
+
+        private bool isClick = false;
+
+        private void OnMouseDown()
+        {
+            if (isClick == false)
+            {
+                StartCoroutine(MouseClick());
+            }
+        }
+
+        private IEnumerator MouseClick()
+        {
+            OnMouseClick?.Invoke();
+
+            isClick = true;
+
+            yield return new WaitForSeconds(DelayClick);
+
+            isClick = false;
+        }
+    }
+}
---
+++ Assets/_MyScripts/Events/SetAnimationBool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyEngine
+{
+    // устанавливает булевскую переменную в аниматоре
+    public class SetAnimationBool : MonoBehaviour
+    {
+        public string VariableName;
+        public bool Value;
+        public Animator Anim;
+
+        private void Start()
+        {
+            if (Anim == null)
+                Anim = GetComponentInChildren<Animator>();
+            if (Anim == null)
+                Debug.LogError("Не найден Animator", this);
+        }
+
+        public void SetBool()
+        {
+            if (Anim == null)
+                return;
+
+            Anim.SetBool(VariableName, Value);
+        }
+
+        public void SetBoolTrue()
+        {
+            if (Anim == null)
+                return;
+
+            Anim.SetBool(VariableName, true);
+        }
+
+        public void SetBoolFalse()
+        {
+            if (Anim == null)
+                return;
+
+            Anim.SetBool(VariableName, false);
+        }
+    }
+}
